865fdb98500fed1994e2815deadc680822df7b5e
Mauro Chojrin <mauro.chojrin@leewayweb.com>
2022-03-10 17:23:10 +0100

    Added unique constraint to give aways to make sure only one user can win at any given day

---
+++ api/migrations/Version20220310161425.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20220310161425 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_FB761D7CAA9E377A ON give_away (date)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE SCHEMA public');
+        $this->addSql('DROP INDEX UNIQ_FB761D7CAA9E377A');
+    }
+}
