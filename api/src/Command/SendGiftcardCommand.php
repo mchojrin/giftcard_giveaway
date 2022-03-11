3813a5c320e7d4c3abc958978a937e074a7aa628
Mauro Chojrin <mauro.chojrin@leewayweb.com>
2022-03-11 14:55:37 +0100

    Added a command to purchase the gitfcards and send them to the winners

---
+++ api/src/Command/SendGiftcardCommand.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Command;
+
+use App\Reloadly\Client;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+#[AsCommand(
+    name: 'app:send-giftcard',
+    description: 'Send the giftcard to the winners',
+)]
+class SendGiftcardCommand extends Command
+{
+    private EntityManagerInterface $entityManager;
+    private Client $client;
+
+    public function __construct(EntityManagerInterface $entityManager, Client $client, string $name = null)
+    {
+        parent::__construct($name);
+        $this->entityManager = $entityManager;
+        $this->client = $client;
+    }
+
+    protected function configure(): void
+    {
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $io = new SymfonyStyle($input, $output);
+
+        $this->client->sendGiftCard('mchojrin@gmail.com');
+
+        $io->success('You have a new command! Now make it your own! Pass --help to see your options.');
+
+        return Command::SUCCESS;
+    }
+}
